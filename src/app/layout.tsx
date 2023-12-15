523d60f209a688c393716dde64192b409767c122
NahimMora <nahimsalta@gmail.com>
2023-12-15 19:50:30 -0300

    Navbar terminada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Notification from '@/Components/Notification'
+import Navbar from '@/Components/Navbar'
+import Footer from '@/Components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Notification/>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   )
 }
